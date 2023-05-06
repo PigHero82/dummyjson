83307cb44be3737c898e348f15da5655df2efb11
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2023-05-06 21:42:42 +0800

    Update Product (Add Product Page)

---
+++ src/pages/products/add.tsx
@@ -0,0 +1,83 @@
+// React
+import { Fragment } from "react"
+
+// Next
+import Head from "next/head"
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+// API
+import { api } from "@/services"
+
+// Component
+import { Layout } from "@/components"
+import { FormSection } from "@/components/products"
+
+// Form
+import { Formik } from "formik"
+import * as yup from 'yup'
+
+// Third-Party Library
+import { ChevronBackOutline } from "react-ionicons"
+import { toast } from "react-hot-toast"
+
+export default function AddProduct() {
+  // Hooks
+  const router = useRouter()
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add Product</title>
+      </Head>
+
+      <Layout>
+        <main className="container mx-auto">
+          <section className="flex justify-between items-center">
+            <div className="prose">
+              <h2 className="m-0">Add Product</h2>
+              <div className="text-gray">Data will not changed in the server</div>
+            </div>
+
+            <Link href="/products">
+              <button type='button' className="btn btn-outline focus:loading">
+                <ChevronBackOutline color='#00000' /> Back
+              </button>
+            </Link>
+          </section>
+
+          <Formik
+            initialValues={{
+              title: "",
+              description: "asdasd",
+              price: null,
+              brand: "",
+              category: ""
+            }}
+            validationSchema={yup.object().shape({
+              title: yup.string().label("Title").required(),
+              price: yup.number().label("Price").min(0).nullable().required(),
+              brand: yup.string().label("Brand").required(),
+              category: yup.string().label("Category").required()
+            })}
+            onSubmit={values => {
+              toast.promise(
+                api.post("/products/add", values),
+                {
+                  loading: 'Processing...',
+                  success: () => {
+                    router.push('/products')
+                    return "Success"
+                  },
+                  error: "Failed",
+                }
+              )
+            }}
+          >
+            <FormSection />
+          </Formik>
+        </main>
+      </Layout>
+    </Fragment>
+  )
+}
