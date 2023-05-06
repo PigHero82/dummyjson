9382e54c9afc1e4a792d2b8c6eb27b52065ced1a
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2023-05-06 21:34:47 +0800

    Add Toaster Provider into app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
       initial={false}
       onExitComplete={() => window.scrollTo(0, 0)}
     >
+      <Toaster />
       <Component {...pageProps} key={router.asPath} />;
     </AnimatePresence>
   )
