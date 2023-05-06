70b33c928a1aff3af1e2c1281505ca36747c37dc
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2023-05-06 21:50:16 +0800

    Update Index (add Head)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+// React
+import { Fragment } from "react"
+
 // Next
+import Head from "next/head"
 import Link from "next/link"
 
 // Component
@@ -9,29 +13,35 @@ import { motion } from "framer-motion"
 
 export default function Home() {
   return (
-    <Layout>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <div className="container mx-auto h-[90vh] flex flex-col">
-          <div className="my-auto">
-            <div className="text-center md:text-8xl sm:text-7xl text-5xl">
-              dummyJSON
+    <Fragment>
+      <Head>
+        <title>dummyJSON</title>
+      </Head>
+
+      <Layout>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="container mx-auto h-[90vh] flex flex-col">
+            <div className="my-auto">
+              <div className="text-center md:text-8xl sm:text-7xl text-5xl">
+                dummyJSON
+              </div>
+            </div>
+
+            <div className="my-5 flex flex-wrap justify-center items-stretch gap-5">
+              <Link href='/products'>
+                <button type='button' className="btn focus:loading">
+                  Products
+                </button>
+              </Link>
             </div>
           </div>
-
-          <div className="my-5 flex flex-wrap justify-center items-stretch gap-5">
-            <Link href='/products'>
-              <button type='button' className="btn focus:loading">
-                Products
-              </button>
-            </Link>
-          </div>
-        </div>
-      </motion.div>
-    </Layout>
+        </motion.div>
+      </Layout>
+    </Fragment>
   )
 }
