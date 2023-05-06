6ae4768d4a054e9c658b4a8b2212382f3883fac5
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2023-05-06 21:50:07 +0800

    Update Product (add Head)

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,5 +1,9 @@
+// React
+import { Fragment } from 'react'
+
 // Next
 import { GetServerSideProps } from 'next'
+import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 
@@ -8,6 +12,7 @@ import { api } from '@/services'
 
 // Component
 import { Layout } from '@/components'
+import { Layout as ProductLayout } from '@/components/products'
 
 // Form
 import { Field, Form, Formik } from 'formik'
@@ -59,57 +64,63 @@ export default function Product(props: {
   }
 
   return (
-    <Layout>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <main className='container mx-auto'>
-          <section className='mb-5'>
-            <Formik
-              initialValues={initialValues}
-              onSubmit={(values: {
-                search?: string
-              }) => {
-                // @ts-ignore
-                router.push({
-                  pathname: '/',
-                  query: {
-                    ...searchQuery,
-                    search: values.search
-                  }
-                })
-              }}
-            >
-              {({ isSubmitting }) => (
-                <Form>
-                  <div className="form-control">
-                    <div className="input-group">
-                      <Field name="search">
-                        {(props: { field: any }) => <input type="text" className="input input-bordered w-full" placeholder="Search..." {...props.field} />}
-                      </Field>
+    <Fragment>
+      <Head>
+        <title>Product</title>
+      </Head>
 
-                      <button type='submit' title='Submit' className={`btn btn-square ${isSubmitting && "loading"}`}>
-                        {!isSubmitting && <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>}
-                      </button>
-                    </div>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </section>
+      <Layout>
+        <ProductLayout>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <main className='container mx-auto'>
+              <section className='mb-5'>
+                <Formik
+                  initialValues={initialValues}
+                  onSubmit={(values: {
+                    search?: string
+                  }) => {
+                    // @ts-ignore
+                    router.push({
+                      pathname: '/products',
+                      query: {
+                        ...searchQuery,
+                        search: values.search
+                      }
+                    })
+                  }}
+                >
+                  {({ isSubmitting }) => (
+                    <Form>
+                      <div className="form-control">
+                        <div className="input-group">
+                          <Field name="search">
+                            {(props: { field: any }) => <input type="text" className="input input-bordered w-full" placeholder="Search..." {...props.field} />}
+                          </Field>
 
-          <div className='mx-auto flex flex-wrap justify-center gap-5'>
-            {props.data.products.map((val, index) => {
-              // Variables
-              const discountPercentage = val.discountPercentage
-              const discounted = isDiscounted(discountPercentage)
-              const price = val.price
+                          <button type='submit' title='Submit' className={`btn btn-square ${isSubmitting && "loading"}`}>
+                            {!isSubmitting && <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>}
+                          </button>
+                        </div>
+                      </div>
+                    </Form>
+                  )}
+                </Formik>
+              </section>
 
-              return (
-                <div key={index} className='group w-56 flex flex-col justify-stretch'>
+              <div className='mx-auto flex flex-wrap justify-center gap-5'>
+                {props.data.products.map((val, index) => {
+                  // Variables
+                  const discountPercentage = val.discountPercentage
+                  const discounted = isDiscounted(discountPercentage)
+                  const price = val.price
+
+                  return (
+                    <div key={index} className='group w-56 flex flex-col justify-stretch'>
                       <section className='relative'>
                         <img src={val.thumbnail} alt={val.title} className="h-64 object-cover" />
 
@@ -179,59 +190,61 @@ export default function Product(props: {
                         </div>
                       </section>
 
-                  <div className='flex justify-between gap-x-5'>
-                    <h3>{val.title}</h3>
+                      <div className='flex justify-between gap-x-5'>
+                        <h3>{val.title}</h3>
 
-                    <section className='text-end'>
-                      <h3 className={discounted ? 'text-gray-500 line-through' : ''}>${price}</h3>
-                      {discounted && <h3>${(price - (price * discountPercentage / 100)).toFixed(2)}</h3>}
-                    </section>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
+                        <section className='text-end'>
+                          <h3 className={discounted ? 'text-gray-500 line-through' : ''}>${price}</h3>
+                          {discounted && <h3>${(price - (price * discountPercentage / 100)).toFixed(2)}</h3>}
+                        </section>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
 
-          <div className='text-center mt-5'>
-            <div className="btn-group">
-              {[...Array(props.data.total / props.data.limit)].map((_, index) => {
-                // Variables
-                const current = Boolean(((props.data.skip + 20) / props.data.limit) === index + 1)
+              <div className='text-center mt-5'>
+                <div className="btn-group">
+                  {[...Array(props.data.total / props.data.limit)].map((_, index) => {
+                    // Variables
+                    const current = Boolean(((props.data.skip + 20) / props.data.limit) === index + 1)
 
-                return (
-                  <button
-                    key={index}
-                    type='button'
-                    className={`btn btn-sm ${current ? "btn-active" : ""}`}
-                    onClick={() => {
-                      if (!current) {
-                        // @ts-ignore
-                        router.push({
-                          pathname: '/',
-                          query: {
-                            ...searchQuery,
-                            page: index + 1
+                    return (
+                      <button
+                        key={index}
+                        type='button'
+                        className={`btn btn-sm ${current ? "btn-active" : ""}`}
+                        onClick={() => {
+                          if (!current) {
+                            // @ts-ignore
+                            router.push({
+                              pathname: '/products',
+                              query: {
+                                ...searchQuery,
+                                page: index + 1
+                              }
+                            })
                           }
-                        })
-                      }
-                    }}
-                  >
-                    {index + 1}
-                  </button>
-                )
-              })}
-            </div>
-          </div>
-        </main>
-      </motion.div>
-    </Layout>
+                        }}
+                      >
+                        {index + 1}
+                      </button>
+                    )
+                  })}
+                </div>
+              </div>
+            </main>
+          </motion.div>
+        </ProductLayout>
+      </Layout>
+    </Fragment>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { query }: { query: any } = context
-  // Fetch data from external API
-  const res = await fetch(`https://dummyjson.com/products${query.search ? '/search' : ''}?` + new URLSearchParams({
+  
+  const res = await fetch(`${process.env.API_URL}/products${query.search ? '/search' : ''}?` + new URLSearchParams({
     ...query,
     q: query.search,
     skip: query.page ? (query.page * 20) - 20 : 0,
@@ -245,6 +258,5 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 
-  // Pass data to the page via props
   return { props: { data } }
 }
