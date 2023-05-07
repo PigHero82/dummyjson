a309a9cca0da83a456ab2e1460acc3f409078096
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2023-05-07 10:35:09 +0800

    Component (Select)

---
+++ src/components/common/forms/Select.tsx
@@ -0,0 +1,36 @@
+export function Select(props: {
+  data: {
+    value: string | number
+    label: string | number
+  }[]
+  error?: string | false
+  label?: string
+  loading?: boolean
+  placeholder?: string
+  value?: string
+}) {
+  return (
+    <div className="form-control w-full">
+      <label className="label">
+        <span className="label-text">{props.label}</span>
+      </label>
+
+      {props.loading ? (
+        <select title="Loading" className="select w-full" disabled>
+          <option>Loading...</option>
+        </select>
+      ) : (
+        <select className="select select-bordered w-full" defaultValue={props.value} {...props}>
+          <option value="" hidden>{props.placeholder ?? "Select..."}</option>
+          {props.data.map((val, index) => <option key={index} value={val.value}>{val.label}</option>)}
+        </select>
+      )}
+
+      {props.error && (
+        <label className="label">
+          <span className="label-text-alt text-error">{props.error}</span>
+        </label>
+      )}
+    </div>
+  )
+}
