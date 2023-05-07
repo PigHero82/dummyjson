dd138b72fd689e36556cc18e2c07555a463383b9
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2023-05-07 10:17:49 +0800

    Update Product (handle no products & use select on params)

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -18,7 +18,7 @@ import { Layout as ProductLayout } from '@/components/products'
 import { Field, Form, Formik } from 'formik'
 
 // Third-Party Library
-import { ArrowForwardOutline, PencilOutline, TrashBinOutline } from 'react-ionicons'
+import { ArrowForwardOutline, BagRemoveOutline, PencilOutline, TrashBinOutline } from 'react-ionicons'
 import { motion } from "framer-motion"
 
 // Utility
@@ -112,127 +112,139 @@ export default function Product(props: {
                 </Formik>
               </section>
 
-              <div className='mx-auto flex flex-wrap justify-center gap-5'>
-                {props.data.products.map((val, index) => {
-                  // Variables
-                  const discountPercentage = val.discountPercentage
-                  const discounted = isDiscounted(discountPercentage)
-                  const price = val.price
+              {props.data.products.length === 0 ? (
+                <div className='my-10 text-center'>
+                  <div className='flex justify-center'>
+                    <BagRemoveOutline width="100px" height="100px" />
+                  </div>
 
-                  return (
-                    <div key={index} className='group w-56 flex flex-col justify-stretch'>
-                      <section className='relative'>
-                        <img src={val.thumbnail} alt={val.title} className="h-64 object-cover" />
-
-                        <div className='group-hover:transition-all invisible group-hover:visible absolute top-0 right-0 m-2'>
-                          <div className='btn-group'>
-                            <button type="button" title="Edit" className="group btn btn-sm btn-square btn-warning focus:loading">
-                              <Link href={`/products/edit/${val.id}`}>
-                                <div className='group-focus:hidden'>
-                                  <PencilOutline />
-                                </div>
-                              </Link>
-                            </button>
-
-                            <button
-                              type="button"
-                              title="Edit"
-                              className="btn btn-sm btn-square btn-error"
-                              onClick={() => {
-                                toast((t) => (
-                                  <div className='flex items-center gap-x-3'>
-                                    <div>Are you sure?</div>
-
-                                    <div className='flex'>
-                                      <button
-                                        type='button'
-                                        className='btn btn-sm btn-primary rounded-none'
-                                        onClick={() => {
-                                          toast.dismiss(t.id)
-
-                                          toast.promise(
-                                            api.delete(`/products/${val.id}`),
-                                            {
-                                              loading: 'Processing...',
-                                              success: () => {
-                                                window.location.reload()
-                                                return "Success"
-                                              },
-                                              error: "Failed",
-                                            }
-                                          )
-                                        }}
-                                      >
-                                        Confirm
-                                      </button>
-
-                                      <button type='button' className='btn btn-sm btn-red-500 rounded-none' onClick={() => toast.dismiss(t.id)}>
-                                        Cancel
-                                      </button>
-                                    </div>
-                                  </div>
-                                ))
-                              }}
-                            >
-                              <div className='group-focus:hidden'>
-                                <TrashBinOutline />
-                              </div>
-                            </button>
-                          </div>
-                        </div>
-
-                        <div className='group-hover:transition-all invisible group-hover:visible absolute bottom-0 w-full p-3'>
-                          <Link key={index} href={`/products/detail/${val.id}`}>
-                            <button type='button' className="w-full btn btn-light dark:btn-dark gap-2 focus:loading">
-                              Show Detail <ArrowForwardOutline color='#00000' />
-                            </button>
-                          </Link>
-                        </div>
-                      </section>
-
-                      <div className='flex justify-between gap-x-5'>
-                        <h3>{val.title}</h3>
-
-                        <section className='text-end'>
-                          <h3 className={discounted ? 'text-gray-500 line-through' : ''}>${price}</h3>
-                          {discounted && <h3>${(price - (price * discountPercentage / 100)).toFixed(2)}</h3>}
-                        </section>
-                      </div>
-                    </div>
-                  )
-                })}
-              </div>
-
-              <div className='text-center mt-5'>
-                <div className="btn-group">
-                  {[...Array(props.data.total / props.data.limit)].map((_, index) => {
+                  <div className='text-2xl font-bold'>No Products Available</div>
+                </div>
+              ) : (
+                <div className='mx-auto flex flex-wrap justify-center gap-5'>
+                  {props.data.products.map((val, index) => {
                     // Variables
-                    const current = Boolean(((props.data.skip + 20) / props.data.limit) === index + 1)
+                    const discountPercentage = val.discountPercentage
+                    const discounted = isDiscounted(discountPercentage)
+                    const price = val.price
 
                     return (
-                      <button
-                        key={index}
-                        type='button'
-                        className={`btn btn-sm ${current ? "btn-active" : ""}`}
-                        onClick={() => {
-                          if (!current) {
-                            // @ts-ignore
-                            router.push({
-                              pathname: '/products',
-                              query: {
-                                ...searchQuery,
-                                page: index + 1
-                              }
-                            })
-                          }
-                        }}
-                      >
-                        {index + 1}
-                      </button>
+                      <div key={index} className='group w-56 flex flex-col justify-stretch'>
+                        <section className='relative'>
+                          <img src={val.thumbnail} alt={val.title} className="h-64 object-cover" />
+
+                          <div className='group-hover:transition-all invisible group-hover:visible absolute top-0 right-0 m-2'>
+                            <div className='btn-group'>
+                              <button type="button" title="Edit" className="group btn btn-sm btn-square btn-warning focus:loading">
+                                <Link href={`/products/edit/${val.id}`}>
+                                  <div className='group-focus:hidden'>
+                                    <PencilOutline />
+                                  </div>
+                                </Link>
+                              </button>
+
+                              <button
+                                type="button"
+                                title="Edit"
+                                className="btn btn-sm btn-square btn-error"
+                                onClick={() => {
+                                  toast((t) => (
+                                    <div className='flex items-center gap-x-3'>
+                                      <div>Are you sure?</div>
+
+                                      <div className='flex'>
+                                        <button
+                                          type='button'
+                                          className='btn btn-sm btn-primary rounded-none'
+                                          onClick={() => {
+                                            toast.dismiss(t.id)
+
+                                            toast.promise(
+                                              api.delete(`/products/${val.id}`),
+                                              {
+                                                loading: 'Processing...',
+                                                success: () => {
+                                                  window.location.reload()
+                                                  return "Success"
+                                                },
+                                                error: "Failed",
+                                              }
+                                            )
+                                          }}
+                                        >
+                                          Confirm
+                                        </button>
+
+                                        <button type='button' className='btn btn-sm btn-red-500 rounded-none' onClick={() => toast.dismiss(t.id)}>
+                                          Cancel
+                                        </button>
+                                      </div>
+                                    </div>
+                                  ))
+                                }}
+                              >
+                                <div className='group-focus:hidden'>
+                                  <TrashBinOutline />
+                                </div>
+                              </button>
+                            </div>
+                          </div>
+
+                          <div className='group-hover:transition-all invisible group-hover:visible absolute bottom-0 w-full p-3'>
+                            <Link key={index} href={`/products/detail/${val.id}`}>
+                              <button type='button' className="w-full btn btn-light dark:btn-dark gap-2 focus:loading">
+                                Show Detail <ArrowForwardOutline color='#00000' />
+                              </button>
+                            </Link>
+                          </div>
+                        </section>
+
+                        <div className='flex justify-between gap-x-5'>
+                          <h3>{val.title}</h3>
+
+                          <section className='text-end'>
+                            <h3 className={discounted ? 'text-gray-500 line-through' : ''}>${price}</h3>
+                            {discounted && <h3>${(price - (price * discountPercentage / 100)).toFixed(2)}</h3>}
+                          </section>
+                        </div>
+                      </div>
                     )
                   })}
                 </div>
-              </div>
+              )}
+
+              {props.data.products.length > 0 && (
+                <div className='text-center mt-5'>
+                  <div className="btn-group">
+                    {[...Array(props.data.total / props.data.limit ?? 0)].map((_, index) => {
+                      // Variables
+                      const current = Boolean(((props.data.skip + 20) / props.data.limit) === index + 1)
+
+                      return (
+                        <button
+                          key={index}
+                          type='button'
+                          className={`btn btn-sm ${current ? "btn-active" : ""}`}
+                          onClick={() => {
+                            if (!current) {
+                              // @ts-ignore
+                              router.push({
+                                pathname: '/products',
+                                query: {
+                                  ...searchQuery,
+                                  page: index + 1
+                                }
+                              })
+                            }
+                          }}
+                        >
+                          {index + 1}
+                        </button>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
             </main>
           </motion.div>
         </ProductLayout>
@@ -246,17 +258,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   
   const res = await fetch(`${process.env.API_URL}/products${query.search ? '/search' : ''}?` + new URLSearchParams({
     ...query,
-    q: query.search,
+    limit: query.limit ?? 20,
+    select: "discountPercentage,id,price,thumbnail,title",
     skip: query.page ? (query.page * 20) - 20 : 0,
-    limit: query.limit ?? 20
+    q: query.search
   }))
   const data = await res.json()
-
-  if (data.products.length === 0) { 
-    return {
-      notFound: true
-    }
-  }
 
   return { props: { data } }
 }
